6342b0dbdb668895c4613aa1f4efc29618be5388
James E Brown <brownje0610@gmail.com>
2024-11-13 16:18:35 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 # add your code here
 for n in range(1, 5001):
-    if n % 3 == 0 and i % 5 == 0:
+    if n % 3 == 0 and n % 5 == 0:
         print("FizzBuzz")
     elif n % 3 == 0:
         print("Fizz")
