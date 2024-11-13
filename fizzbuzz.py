bdfb777d3ab5881fee07aa6c23afc34aa2a8cc45
James E Brown <brownje0610@gmail.com>
2024-11-13 16:13:25 -0500

    Updated with my project code for fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
 # add your code here
-
+for n in range(1, 5001):
+    if n % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
